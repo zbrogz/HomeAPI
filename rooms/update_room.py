8f893e648eecd137e94daac42d9280c789e7ff14
Christopher Slade <crslade@gmail.com>
2017-09-29 16:26:52 -0600

    Added Update room

--- rooms/update_room.py
+++ rooms/update_room.py
@@ -2,31 +2,29 @@ import os
 import boto3
 import json
 from datetime import datetime
-from uuid import uuid4 as uuid
+from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    print(" Starting Create Room Lambda Function")
+    print(" Starting Update Room Lambda Function")
     
     table_name = os.environ['ROOMS_TABLE_NAME']
     room_table = boto3.resource('dynamodb').Table(table_name)
     
     eventBody = json.loads(event['body'])
-    uid = uuid().hex
+
     nowtime = datetime.now().strftime('%x-%X')
-    print("UID is "+uid)
     
-    if 'roomName' in eventBody:
-        room = {
-            'uuid': uid,
-            'roomName': eventBody['roomName'],
-            'created_at': nowtime,
-            'updated_at': nowtime
-        }
-        room_table.put_item(Item=room)
+    if 'roomName' in eventBody and event['pathParameters'] and 'roomID' in event['pathParameters']:
+        uuid = event['pathParameters']['roomID']
+
+        result = room_table.update_item(
+              Key={'uuid': uuid},
+              UpdateExpression="set roomName = :n",
+              ExpressionAttributeValues={':n': eventBody['roomName']})
         response = {
             "isBase64Encoded": "false",
             "statusCode": 200,
-            "body": json.dumps(room)
+            "body": "{\"message\": \"Room updated\"}"
         }
     else:
         response = {
