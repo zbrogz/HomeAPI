3f0e8233ce5a661e258d60b4617fa3646d16e6fa
Christopher Slade <crslade@gmail.com>
2017-10-02 15:39:51 -0600

    Updated indent size for consistency

--- rooms/get_rooms.py
+++ rooms/get_rooms.py
@@ -5,29 +5,29 @@ from datetime import datetime
 #from uuid import uuid4 as uuid
 
 def get_all_rooms():
-    print("Getting Rooms")
-    
-    table_name = os.environ['ROOMS_TABLE_NAME']
-    rooms_table = boto3.resource('dynamodb').Table(table_name)
-    
-    
-    result = rooms_table.scan()
-    rooms = result['Items']
-    while 'LastEvaluateKey' in result:
-            result = rooms_table.scan(ExclusiveStartKey=result['LastEvaluateKey'])
-            rooms += result['Items']
-            
-    return rooms
+  print("Getting Rooms")
+  
+  table_name = os.environ['ROOMS_TABLE_NAME']
+  rooms_table = boto3.resource('dynamodb').Table(table_name)
+  
+  
+  result = rooms_table.scan()
+  rooms = result['Items']
+  while 'LastEvaluateKey' in result:
+          result = rooms_table.scan(ExclusiveStartKey=result['LastEvaluateKey'])
+          rooms += result['Items']
+          
+  return rooms
 
 def lambda_handler(event, context):
-    print(" Starting Get Rooms Lambda Function")
-    
-    rooms = get_all_rooms()
-    
-    response = {
-        "isBase64Encoded": "false",
-        "statusCode": 200,
-        "body": json.dumps(rooms)
-    }
-    
-    return response
+  print(" Starting Get Rooms Lambda Function")
+  
+  rooms = get_all_rooms()
+  
+  response = {
+      "isBase64Encoded": "false",
+      "statusCode": 200,
+      "body": json.dumps(rooms)
+  }
+  
+  return response
--- rooms/update_room.py
+++ rooms/update_room.py
@@ -5,33 +5,33 @@ from datetime import datetime
 from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    print(" Starting Update Room Lambda Function")
-    
-    table_name = os.environ['ROOMS_TABLE_NAME']
-    room_table = boto3.resource('dynamodb').Table(table_name)
-    
-    eventBody = json.loads(event['body'])
+  print(" Starting Update Room Lambda Function")
+  
+  table_name = os.environ['ROOMS_TABLE_NAME']
+  room_table = boto3.resource('dynamodb').Table(table_name)
+  
+  eventBody = json.loads(event['body'])
 
-    nowtime = datetime.now().strftime('%x-%X')
-    
-    if 'roomName' in eventBody and event['pathParameters'] and 'roomID' in event['pathParameters']:
-        uuid = event['pathParameters']['roomID']
+  nowtime = datetime.now().strftime('%x-%X')
+  
+  if 'roomName' in eventBody and event['pathParameters'] and 'roomID' in event['pathParameters']:
+      uuid = event['pathParameters']['roomID']
 
-        result = room_table.update_item(
-              Key={'uuid': uuid},
-              UpdateExpression="set roomName = :n",
-              ExpressionAttributeValues={':n': eventBody['roomName']})
-        response = {
-            "isBase64Encoded": "false",
-            "statusCode": 200,
-            "body": "{\"message\": \"Room updated\"}"
-        }
-    else:
-        response = {
-            "isBase64Encoded": "false",
-            "statusCode": 400,
-            "body": "{\"errorMessage\": \"Invalid Parameters: Missing roomName\"}"
-        }
+      result = room_table.update_item(
+            Key={'uuid': uuid},
+            UpdateExpression="set roomName = :n",
+            ExpressionAttributeValues={':n': eventBody['roomName']})
+      response = {
+          "isBase64Encoded": "false",
+          "statusCode": 200,
+          "body": "{\"message\": \"Room updated\"}"
+      }
+  else:
+      response = {
+          "isBase64Encoded": "false",
+          "statusCode": 400,
+          "body": "{\"errorMessage\": \"Invalid Parameters: Missing roomName\"}"
+      }
 
-    
-    return response
+  
+  return response
